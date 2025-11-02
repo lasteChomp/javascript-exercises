68de685fea7f093410d292a4d5c93c0dbfb38bc5
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-02 22:46:01 +0300

    feat(removeFromArray.js): Code the removeFromArray function in the eigth exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...params) {
+  const newArray = [];
+
+  array.forEach((item) => {
+    if (!params.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
+  //return array.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
