2886a57e4f78cdfebe243dbecb34db9c5597ae35
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-02 19:29:19 +0300

    Code the repeatString function in the sixth exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, repeatTimes) {
+  if (repeatTimes < 0) {
+    return "ERROR";
+  }
 
+  let repeatedString = "";
+  for (let i = 0; i < repeatTimes; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
