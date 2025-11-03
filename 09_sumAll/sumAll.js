9fe4b860d397e8bb18a77cb7cf5e81349b75587f
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-03 10:24:07 +0300

    feat(sumAll.js): Code the sumAll function in the ninth exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  }
 
+  let smallNum = num1 < num2 ? num1 : num2;
+  const bigNum = num1 < num2 ? num2 : num1;
+
+  let sum = 0;
+  for (smallNum; smallNum <= bigNum; smallNum++) {
+    sum += smallNum;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
