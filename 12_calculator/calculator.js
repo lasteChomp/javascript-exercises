f88625a209b14eaeca476584c2f7768a31a78766
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-15 18:57:55 +0300

    feat(calculator.js): Code all the functions in the twelveth exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, value) => {
+    return sum + value;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((result, value) => result * value);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
