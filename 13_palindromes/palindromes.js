2085fd92ee2acb16137db900044fdf087d18e61b
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-15 22:13:48 +0300

    feat(palindrome.js): code palindromes function in thirteenth exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const fixedString = string
+    .replace(/[.,!?;:]/g, "") // replace all punctuations with empty string
+    .replace(/\s/g, "") // replace all whitespaces with empty string
+    .toLowerCase(); // make all characters lower case
 
+  let newString = "";
+  for (let i = 0; i < fixedString.length; i++) {
+    if (fixedString[i] === fixedString[fixedString.length - 1 - i]) {
+      newString += fixedString[i];
+    }
+  }
+
+  return newString === fixedString ? true : false;
 };
 
 // Do not edit below this line
