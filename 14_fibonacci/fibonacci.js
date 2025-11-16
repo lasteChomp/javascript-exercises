67a92356aa23b18512d479916fb388c7a9e51c3b
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-16 15:23:32 +0300

    feat(fibonacci.js): code fibonacci function in fourteenth exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (param) {
+  let index = Number(param);
 
+  if (index < 0) {
+    return "OOPS";
+  }
+
+  let fibonacci = [0, 1];
+
+  for (let i = 0; i <= index - 2; i++) {
+    let newMember = fibonacci[i] + fibonacci[i + 1];
+    fibonacci.push(newMember);
+  }
+
+  return fibonacci[index];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+/* 
+0, 1, 
+*/
