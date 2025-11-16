2b3a2566bcb3b64af74a8af64b14b8441682d3ff
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-16 16:10:12 +0300

    feat(findTheOldest.js): code findTheOldest function in sixteenth exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people
+    .map((person) => {
+      if (!person.hasOwnProperty("yearOfDeath")) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
 
+      return {
+        name: person.name,
+        lifeSpan: person.yearOfDeath - person.yearOfBirth,
+      };
+    })
+    .reduce((prev, current) => {
+      if (prev.lifeSpan > current.lifeSpan) {
+        return prev;
+      }
+      return current;
+    }, {});
 };
 
 // Do not edit below this line
